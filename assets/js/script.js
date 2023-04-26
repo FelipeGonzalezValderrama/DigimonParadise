30a7e5b780245cbc2171aa3e257ba78e08d8ee4f
Felipe Gonzalez <felipe.gonzalez.valderrama@gmail.com>
2023-04-26 09:41:51 -0400

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,58 @@
+
+var contenido = document.querySelector("#contenido");
+var searchInput = document.querySelector("#search-input");
+var searchButton = document.querySelector("#search-button");
+
+//cambia el color del navbar
+$(document).ready(function () {
+    $(".navbar").hover(function () {
+        $(this).css({ 'background-color': '#ef3535' });
+    },
+        function () {
+            $(this).css({ 'background-color': '' });
+        });
+});
+
+//fetch consumo de API
+fetch('https://digimon-api.vercel.app/api/digimon')
+    .then(response => response.json())
+    .then(datos => {
+        tabla(datos)
+    });
+
+function tabla(datos) {
+    contenido.innerHTML = ""
+    let contador = 1;
+    for (let temp of datos) {
+        contenido.innerHTML += `      
+        <tr>
+            <th scope="row">${contador}</th>
+            <td>${temp.name}</td> 
+            <td><a href="${temp.img}" target="_blank"><img src="${temp.img}" width="50" height="50" class="zoom-img"></a></td>
+            <td>${temp.level}</td>
+        </tr>
+        `
+        contador++;
+        if (contador > 20) {
+            break;
+        }
+    }
+}
+//Search API
+function searchDigimon() {
+    var searchTerm = searchInput.value.toLowerCase();
+    fetch('https://digimon-api.vercel.app/api/digimon')
+        .then(response => response.json())
+        .then(datos => {
+            var filteredData = datos.filter(function (digimon) {
+                return digimon.name.toLowerCase().includes(searchTerm);
+            });
+            tabla(filteredData);
+        });
+}
+
+searchButton.addEventListener('click', searchDigimon);
+
+
+
+
